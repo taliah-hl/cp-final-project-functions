9d8ceba92df190e86ec8c25822251f204d431892
chiuycp <107933514+chiuycp@users.noreply.github.com>
2025-06-05 18:53:51 +0800

    Update piMain.py
    add connection test

--- piMain.py
+++ piMain.py
@@ -2,17 +2,28 @@ from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
 import os
 import time
 import json
+"""
 import pygame
 from piModules.Recorder import Recorder
 from piModules.Picam    import MyPicam
 from AwsBot import AwsBot
 from pathlib import Path
-
+"""
 base_dir = os.path.dirname(os.path.abspath(__file__))
 # 需要用新的換掉
-THING_NAME = "ResPiForFinal"
-ENDPOINT = "a2rwg7fxsn0b1i-ats.iot.us-east-1.amazonaws.com"
+THING_NAME = "Final_pi_test"
+ENDPOINT = "a2l2r8z087kz73-ats.iot.ap-southeast-2.amazonaws.com"
 
+def report_state(state_name, state):
+    reported_payload = {
+        "state": {
+            "reported": {
+                state_name: state
+            }
+        }
+    }
+    device_shadow.shadowUpdate(json.dumps(reported_payload), None, 5)
+    print(f"{state_name}: {state} reported")
 
 def handle_delta_callback(payload, response_status, token):
     #print("收到 Shadow delta 訊息：")
@@ -24,79 +35,52 @@ def handle_delta_callback(payload, response_status, token):
 
         if notification_state:
             try:
-                pygame.mixer.music.stop()
+                #pygame.mixer.music.stop()
                 # 回報狀態為 reported
-                reported_payload = {
-                    "state": {
-                        "reported": {
-                            "notification": notification_state
-                        }
-                    }
-                }
-                device_shadow.shadowUpdate(json.dumps(reported_payload), None, 5)
+                report_state("notification", notification_state)
             except:
                 pass
-            pygame.mixer.music.load(str(can_dir / 'ActivitiesNotice.mp3'))
-            pygame.mixer.music.play(start=4.0)
+            #pygame.mixer.music.load(str(can_dir / 'ActivitiesNotice.mp3'))
+            #pygame.mixer.music.play(start=4.0)
             
         if(cue_state == "Cue me"):
-            rec.start()
-            picam.capture(img_dir / 'img.jpg')
+            #rec.start()
+            #picam.capture(img_dir / 'img.jpg')
             # 回報狀態為 reported
-            reported_payload = {
-                "state": {
-                    "reported": {
-                        "cue": cue_state
-                    }
-                }
-            }
-            device_shadow.shadowUpdate(json.dumps(reported_payload), None, 5)
+            report_state("cue", cue_state)
+            
         elif(cue_state == "Cue ok"):
             msg               = ''
             
-            rec.stop()
+            #rec.stop()
             
             try:
-                pygame.mixer.music.stop()
+                #pygame.mixer.music.stop()
                 # 回報狀態為 reported
-                reported_payload = {
-                    "state": {
-                        "reported": {
-                            "cue": cue_state
-                        }
-                    }
-                }
-                device_shadow.shadowUpdate(json.dumps(reported_payload), None, 5)
+                report_state("cue", cue_state)
             except:
                 pass
+            """
             interlude = pygame.mixer.Sound(str(can_dir / 'Interlude.mp3'))
             # pygame.mixer.music.load(str(can_dir / 'Interlude.mp3'))
             interlude.set_volume(0.3)
             interlude.play(loops=-1)
             
-            msg = bot.speech_to_text(rec_dir / 'record.wav')
-            print(f"Speech content   : {msg}")
-            
-            
-            res_text = bot.image_to_response(msg, str(img_dir / 'img.jpg'), str(wk_dir / 'chat_record.txt'))
-            print(f"Response : {res_text}")
-            # print(res_text)
-            
-            bot.text_to_speech(res_text, sph_dir / 'response.mp3')
             
             interlude.stop()
             pygame.mixer.music.load(str(sph_dir / 'response.mp3'))
             pygame.mixer.music.play()
-            
+            """
             # # Wait until the sound finishes
             # while pygame.mixer.music.get_busy():
             #     pygame.time.Clock().tick(10)
-        
+            
     except Exception as e:
         print("[Delta Handler] 發生錯誤:", e)
 
 if __name__ == "__main__":
     # 建立工作目錄與元件
+    """
     wk_dir = Path(os.path.dirname(__file__))
     rec_dir = wk_dir / 'tmp/userRecording'
     sph_dir = wk_dir / 'tmp/responseSpeech'
@@ -109,17 +93,27 @@ if __name__ == "__main__":
     picam = MyPicam()
     picam.start()
     pygame.mixer.init()
-
+    """
+    cert_files = {
+        "root_ca": os.path.join(base_dir, "connect_device_package/root-CA.crt"),
+        "private_key": os.path.join(base_dir, "connect_device_package/Final_pi_test.private.key"),
+        "cert": os.path.join(base_dir, "connect_device_package/Final_pi_test.cert.pem")
+    }
     # 建立 Device Shadow 連線
     shadow_client = AWSIoTMQTTShadowClient("DeviceController")
     shadow_client.configureEndpoint(ENDPOINT, 8883)
-    shadow_client.configureCredentials("root-CA.crt", "device.key", "device.crt")
+    shadow_client.configureCredentials(
+        cert_files["root_ca"],
+        cert_files["private_key"],
+        cert_files["cert"]
+    )
     shadow_client.connect()
 
     device_shadow = shadow_client.createShadowHandlerWithName(THING_NAME, True)
 
     # 訂閱 delta
     device_shadow.shadowRegisterDeltaCallback(handle_delta_callback)
+    report_state("rpi", "connected")
 
     print("等待指令中...")
     try:
@@ -127,5 +121,6 @@ if __name__ == "__main__":
             time.sleep(1)
     except KeyboardInterrupt:
         print("中止程式")
-        picam.close()
+        report_state("rpi", "disconnected")
+        #picam.close()
 
